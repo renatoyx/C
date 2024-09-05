a847d89a4fd64189b42b9aa540f3486c372fc22d
renatvg <163028483+renatvg@users.noreply.github.com>
2024-09-05 08:19:19 -0300

    aaa

---
+++ Aula 08/aaaaa.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main()
+{
+    int numero, x; 
+    char resp; 
+    resp = 'S';
+    
+
+    do { 
+         printf("Digite um número qualquer: \n"); 
+        scanf("%d", &numero);   
+
+        x =  numero *3;
+    
+
+        printf("%d \n", x); 
+
+        printf("Deseja continuar? S/N \n"); 
+        scanf("%c", resp); 
+        } while (resp = 'S'); 
+    }  
