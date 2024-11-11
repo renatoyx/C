4a6da5bf5d7c334b86e7f384d0f4661288d7ccd6
renatvg <163028483+renatvg@users.noreply.github.com>
2024-11-11 09:43:43 -0300

    Update bit++.cpp

--- problems/bit++.cpp
+++ problems/bit++.cpp
@@ -1,10 +1,10 @@
 // https://codeforces.com/problemset/problem/282/A
 
 
-#include <bits/stdc++.h>
-using namespace std; 
-#define _ ios_base::sync_with_stdio(0);cin.tie(0); 
-#define endl "\n"; 
+    #include <bits/stdc++.h>
+    using namespace std; 
+    #define _ ios_base::sync_with_stdio(0);cin.tie(0); 
+    #define endl "\n"; 
 int main() { _ 
     int n; cin >> n; 
     int sum = 0;
